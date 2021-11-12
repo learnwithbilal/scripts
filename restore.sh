7db9510e23aae2ca5be267f99ef2c8ec08598186
Bilal <48978036+learnwithbilal@users.noreply.github.com>
2021-11-12 13:48:31 -0500

    Update restore.sh

--- restore.sh
+++ restore.sh
@@ -1,1 +1,5 @@
 bench --site [sitename] --force restore [path to database backup file] --with-private-files [relative-path-to-private-files-backup-file] --with-public-files [relative-path-to-public-files-backup-file]
+
+#Restoring only the database
+
+bench --site [sitename] --force restore [path to database backup file]
