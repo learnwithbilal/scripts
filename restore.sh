b6e0a7c7a84494c4c82b53ddb730a689dca7c1dd
Bilal <48978036+learnwithbilal@users.noreply.github.com>
2021-11-12 13:47:27 -0500

    Create restore.sh

---
+++ restore.sh
@@ -0,0 +1,1 @@
+bench --site [sitename] --force restore [path to database backup file] --with-private-files [relative-path-to-private-files-backup-file] --with-public-files [relative-path-to-public-files-backup-file]
